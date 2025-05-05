85fc429f4c2f970475a01ab0ad0843b202f71365
Dhruv Shah <dhruvshah428@gmail.com>
2025-05-05 23:34:09 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div class="text-5xl font-bold underline">
+        Hello World!
+      </div>
+    </>
+  )
+}
+
+export default App
